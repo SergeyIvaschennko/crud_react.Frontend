143bc956b9f769205240fec5797aec0807528629
SergeyIvaschennko <boss.awdr@mail.ru>
2024-02-27 15:36:44 +0300

    Made GET players on the Home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,4 +1,20 @@
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import { Link, useParams } from "react-router-dom";
 export default function Home() {
+    const [players, setPlayer] = useState([]);
+
+    const { id } = useParams();
+
+    useEffect(() => {
+        loadPlayer();
+    }, []);
+
+    const loadPlayer = async () => {
+        const result = await axios.get("http://localhost:8080/player");
+        setPlayer(result.data);
+    };
+
     return (
         <div className="container">
             <div className="py-4">
@@ -13,17 +29,20 @@ export default function Home() {
                     </tr>
                     </thead>
                     <tbody>
+                    {players.map((player, index) => (
                         <tr>
-                            <th>
+                            <th scope="row" key={index}>
+                                {index + 1}
                             </th>
-                            <td></td>
-                            <td></td>
-                            <td></td>
+                            <td>{player.name}</td>
+                            <td>{player.lastname}</td>
+                            <td>{player.age}</td>
                             <td>
                                 <button>
                                 </button>
                             </td>
                         </tr>
+                    ))}
                     </tbody>
                 </table>
             </div>

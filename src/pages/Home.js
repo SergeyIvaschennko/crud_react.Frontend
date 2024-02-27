0ce6990dbb0e5d1775e92dd404441dd730098269
SergeyIvaschennko <boss.awdr@mail.ru>
2024-02-27 15:27:21 +0300

    Filled simple Home page Add Navbar

---
+++ src/pages/Home.js
@@ -0,0 +1,32 @@
+export default function Home() {
+    return (
+        <div className="container">
+            <div className="py-4">
+                <table className="table border shadow">
+                    <thead>
+                    <tr>
+                        <th scope="col">ID</th>
+                        <th scope="col">Name</th>
+                        <th scope="col">Lastname</th>
+                        <th scope="col">Age</th>
+                        <th scope="col">Action</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <th>
+                            </th>
+                            <td></td>
+                            <td></td>
+                            <td></td>
+                            <td>
+                                <button>
+                                </button>
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    );
+}
